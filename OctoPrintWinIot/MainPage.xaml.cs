d295f49d530d8ef0eeb5143f2e699fd589081e94
Martin Sipek <martin.sipek@live.com>
2015-12-26 22:36:10 +0100

    add Project

---
+++ OctoPrintWinIot/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using OctoPrintWinIot.Pages;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+
+// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+
+namespace OctoPrintWinIot
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Start Task that updates time
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Low,
+                    () =>
+                    {
+                        Lbl_Time.Text = DateTime.Now.ToString("HH:mm tt");
+                        Lbl_TimeBtm.Text = DateTime.Now.ToString("HH:mm tt");
+                        Lbl_Date.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+                    });
+                    await Task.Delay(1000);
+                }
+            });
+
+            if (Lbl_Time.Visibility == Visibility.Visible)
+            {
+                Lbl_TimeBtm.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Frame_Main_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+        private void Frame_Main_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+          
+        }
+
+        private void Btn_Home_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame_Main.Visibility = Visibility.Collapsed;
+            Lbl_TimeBtm.Visibility = Visibility.Collapsed;
+            Lbl_Time.Visibility = Visibility.Visible;
+            Lbl_Date.Visibility = Visibility.Visible;
+            
+        }
+
+        private void Btn_ShowStatus_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame_Main.Visibility = Visibility.Visible;
+            Lbl_TimeBtm.Visibility = Visibility.Visible;
+            Lbl_Time.Visibility = Visibility.Collapsed;
+            Lbl_Date.Visibility = Visibility.Collapsed;
+
+            Pages.Page_Status _SP = new Pages.Page_Status();
+            Frame_Main.Navigate(_SP.GetType());
+        }
+
+        private void Btn_ShowVideo_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame_Main.Visibility = Visibility.Visible;
+            Lbl_TimeBtm.Visibility = Visibility.Visible;
+            Lbl_Time.Visibility = Visibility.Collapsed;
+            Lbl_Date.Visibility = Visibility.Collapsed;
+
+            Pages.Page_Camera _CP = new Pages.Page_Camera();
+            Frame_Main.Navigate(_CP.GetType());
+        }
+
+        private void Btn_ShowControl_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Frame_Main.Visibility = Visibility.Visible;
+            Lbl_TimeBtm.Visibility = Visibility.Visible;
+            Lbl_Time.Visibility = Visibility.Collapsed;
+            Lbl_Date.Visibility = Visibility.Collapsed;
+
+            Pages.Page_Control _CrtlP = new Pages.Page_Control();
+            Frame_Main.Navigate(_CrtlP.GetType());
+        }
+    }
+}
---
+++ OctoPrintWinIot/Pages/Page_Status.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web.Provider;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Windows.System.Threading;
+using Windows.UI.ViewManagement;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace OctoPrintWinIot.Pages
+{ 
+
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class Page_Status : Page
+    {
+        public String apiUrl = "msi-octopi.no-ip.org";
+        public String apiKey = "5E6F88D2A38643478B0183C2AFEFFBEA";
+
+        private DispatcherTimer timer;
+
+        public Page_Status()
+        {
+            this.InitializeComponent();
+            //getPrintOperation();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            getPrintOperation();
+            getjobOperation();
+        }
+
+        private async void getPrintOperation()
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://"+ apiUrl +"/api/printer?apikey="+ apiKey;
+            var responses = await client.GetAsync(url);
+
+            responses.EnsureSuccessStatusCode();
+
+            string content = await responses.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(content);
+
+            txtPrintTemp.Text = json["temperature"]["tool0"]["actual"].ToString() + "°C / " + json["temperature"]["tool0"]["target"].ToString() + "°C";
+            txtState.Text = json["state"]["text"].ToString();
+
+        }
+
+        private async void getjobOperation()
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://" + apiUrl + "/api/job?apikey=" + apiKey;
+            var responses = await client.GetAsync(url);
+
+            responses.EnsureSuccessStatusCode();
+
+            string content = await responses.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(content);
+
+            if (json["progress"]["completion"].ToString() == "")
+            {
+                pbProgress.Value = 0;
+            }
+            else
+            {
+                string ProcessStr = json["progress"]["completion"].ToString();
+                double ProcessDouble = double.Parse(ProcessStr);
+                int ProgressBar = Convert.ToInt32(ProcessDouble);
+                pbProgress.Value = ProgressBar;
+            }
+        }
+    }
+}
